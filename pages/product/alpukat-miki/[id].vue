de9027522e62247e95f5f6a849f81236ff9d9896
fl <rizalpratama10010@gmail.com>
2023-03-06 17:06:43 +0700

    feat: product api

---
+++ pages/product/alpukat-miki/[id].vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="bg-primary min-h-screen">
+    <div class="pt-5 pb-20">
+      <div class="max-md:px-10 max-lg:px-20 lg:px-36 z-50">
+        <NSpin stroke="white" :show="pending">
+          <template v-if="res.data && Object.keys(res.data).length > 0">
+            <div
+              class="mt-28 flex justify-between bg-gray-100 pt-5 pb-5 px-5 rounded-xl text-black"
+            >
+              <div class="basis-4/12">
+                <img class="w-full rounded" :src="res.data.attachment.image" />
+              </div>
+              <div class="basis-8/12 flex pl-10">
+                <div>
+                  <div
+                    class="max-md:text-2xl md:text-3xl font-bold border-b-[2px] border-primary md:pt-5 max-md:pb-1 md:pb-2"
+                  >
+                    {{ res.data.content.title }}
+                  </div>
+                  <div class="font-bold max-md:text-sm md:text-base pt-3">
+                    {{ res.data.content.content }}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </template>
+        </NSpin>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { NSpin } from "naive-ui";
+
+const { id } = useRoute().params;
+
+const { API_LIST } = useApiUrl();
+const res = ref<any>({ data: {} });
+const pending = ref(true);
+
+const loadData = async () => {
+  pending.value = true;
+
+  await $fetch<any>(`${API_LIST.GET_PRODUCT_LIST}/${1}/${id}`)
+    .then((val) => {
+      console.log(val.data);
+      res.value.data = val.data;
+    })
+    .finally(() => (pending.value = false));
+};
+
+onMounted(() => loadData());
+</script>
--- pages/product/index.vue
+++ pages/product/index.vue
@@ -7,17 +7,12 @@
           Tersedia
         </h1>
         <div class="grid gap-16 md:grid-cols-2 sm:grid-cols-1">
-          <NuxtLink
-            v-for="(data, index) in datas"
-            to="/product/category"
-            v:key="index"
-            :img="data.img"
-          >
+          <NuxtLink to="/product/alpukat-miki">
             <div class="bg-gray-200 rounded-xl shadow">
               <div
-                class="bg-[url('/gallery/miki.jpg')] w-full h-64 bg-center rounded-xl bg-cover"
+                class="w-full h-64 bg-center rounded-xl bg-cover bg-[url('/miki.jpg')]"
               >
-                <div class="pb-10 absolute relative h-full rounded-xl">
+                <div class="pb-10 relative h-full rounded-xl">
                   <div
                     class="bg-gradient-to-t from-black h-full w-full absolute rounded-xl opacity-60"
                   ></div>
@@ -25,10 +20,33 @@
                     <p
                       class="font-semibold text-white text-xl mb-1 opacity-100"
                     >
-                      {{ data.title }}
+                      Alpukat Miki
                     </p>
-                    <small class="font-medium font-[Poppins] text-white"
-                      >Contact: {{ data.contact }}
+                    <small class="font-medium font-[Poppins] text-white">
+                      Contact: 085881234491 (Andi Setiawan)
+                    </small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </NuxtLink>
+          <NuxtLink to="/product/hasil-kebun">
+            <div class="bg-gray-200 rounded-xl shadow">
+              <div
+                class="w-full h-64 bg-center rounded-xl bg-cover bg-[url('/hasil-kebun.jpg')]"
+              >
+                <div class="pb-10 relative h-full rounded-xl">
+                  <div
+                    class="bg-gradient-to-t from-black h-full w-full absolute rounded-xl opacity-60"
+                  ></div>
+                  <div class="absolute px-5 py-3" style="bottom: 0">
+                    <p
+                      class="font-semibold text-white text-xl mb-1 opacity-100"
+                    >
+                      Hasil Kebun
+                    </p>
+                    <small class="font-medium font-[Poppins] text-white">
+                      Contact: 085736921243 (Sellvya Evitarani)
                     </small>
                   </div>
                 </div>
@@ -41,40 +59,4 @@
   </div>
 </template>
 
-<style scoped></style>
-<script setup>
-let datas = [
-  {
-    title: "Alpukal Miki",
-    contact: "085881234491 (Andi Setiawan)",
-    items: [
-      {
-        img: "/gallery/gallery-fruit.jpg",
-        fruit: "Buah Naga",
-        qty: "Total Buah 10 Buah",
-      },
-      {
-        img: "/gallery/gallery-oranges.jpg",
-        fruit: "Jeruk",
-        qty: "Total Buah 10 Buah",
-      },
-    ],
-  },
-  {
-    title: "Alpukal Miki 2",
-    contact: "085881234491 (Andi Setiawan)",
-    items: [
-      {
-        img: "/gallery/gallery-bananas.jpg",
-        fruit: "Pisang",
-        qty: "Total Buah 10 Buah",
-      },
-      {
-        img: "/gallery/gallery-anggur.jpg",
-        fruit: "Peer",
-        qty: "Total Buah 10 Buah",
-      },
-    ],
-  },
-];
-</script>
+<script setup></script>
